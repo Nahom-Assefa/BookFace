7237ebaabd9e09d52383400b92f6f307285c8393
Nahom Assefa <nzenebe@gmail.com>
2022-02-11 16:30:14 -0600

    user routes framework

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,1 +1,66 @@
-const { User } = require("../models")
+const { User } = require("../models");
+
+const userController = {
+  // get all users
+  getAllUsers(req, res) {
+    User.find({})
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => {
+        console.log(err);
+        res.json(err);
+      });
+  },
+  //get one user
+  getOneUser({ params }, res) {
+    User.findOne({ _id: params.id })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.json({ message: "No user associated with this id!" });
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.json(err);
+      });
+  },
+  // create a user
+  createUser({ body }, res) {
+    User.create({ body })
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => {
+        console.log(err);
+        res.json(err);
+      });
+  },
+  // update user
+  updateUser({ params, body }, res) {
+    User.findOneAndUpdate({ _id: params.id }, body, { new: true })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.json({ message: "No user associated with this id!" });
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.json(err);
+      });
+  },
+  // delete user
+  deleteUser({ params }, res) {
+    User.findOneAndDelete({ _id: params.id })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.json({ message: "No user associated with this id!" });
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => {
+        console.log(err);
+        res.json(err);
+      });
+  },
+};
+
+module.exports = userController;
--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,17 +1,25 @@
 const router = require("express").Router();
 
+const {
+    getAllUsers,
+    createUser,
+    getOneUser,
+    updateUser,
+    deleteUser
+} = require("../../controllers/user-controller")
+
 // /api/users
 router
 .route("/")
-.get()
-.post()
+.get(getAllUsers)
+.post(createUser)
 
 // /api/users/id
 router
 .route("/:id")
-.get()
-.put()
-.delete()
+.get(getOneUser)
+.put(updateUser)
+.delete(deleteUser)
 
 
 // /api/users/:userId/friends/:friendId
